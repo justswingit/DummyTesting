d133f67a1371133376f69331aad06e3c1bedc0b9
justswingit <michael.lamhotingusa@gmail.com>
2023-06-19 15:01:50 -0700

    Hopefully this would also upload the src

--- src/Components/sidebarData.js
+++ src/Components/sidebarData.js
@@ -1,10 +1,10 @@
 import             React   from 'react'           ;
 import   *    as AiIcons   from 'react-icons/ai'  ;
-import   *    as DvIcons   from 'react-icons/di'  ;
+/*import   *    as DvIcons   from 'react-icons/di'  ;
 import   *    as GiIcons   from 'react-icons/gi'  ;
 import   *    as TbIcons   from 'react-icons/tb'  ;
 import   *    as SiIcons   from 'react-icons/si'  ;
-import   *    as BsIcons   from 'react-icons/bs'  ;
+import   *    as BsIcons   from 'react-icons/bs'  ;*/
 
 const SidebarData = [
     {title: "Home",        path: '/',           icon: <AiIcons.AiFillHome/>,               cName: "nav-text"},
